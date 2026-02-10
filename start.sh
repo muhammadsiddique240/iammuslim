25ae3e9773a8887505afa1a051f1bc2d3e637e5f
muhammadsiddiquefab <muhammadsiddiquefab@gmail.com>
2026-02-10 23:45:59 +0500

    Add gunicorn access/error logging for 502 debug

--- start.sh
+++ start.sh
@@ -20,4 +20,6 @@ python manage.py collectstatic --noinput --clear
 exec gunicorn config.wsgi:application \
   --bind "0.0.0.0:${PORT_VALUE}" \
   --workers "${WEB_CONCURRENCY:-3}" \
-  --timeout "${GUNICORN_TIMEOUT:-60}"
+  --timeout "${GUNICORN_TIMEOUT:-60}" \
+  --access-logfile - \
+  --error-logfile -
